bf5a5d1678dec9204fe6dd5288eb5d415dd0abde
Boris Precijan <borisprecijanbp@gmail.com>
2023-01-11 23:08:51 +0100

    Spacer Composable

--- app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
@@ -4,15 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.text.selection.DisableSelection
-import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import uns.pmf.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
 
@@ -29,24 +27,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Example() {
-    SelectionContainer {
-        Column(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Text(
-                text = "This text is selectable",
-                modifier = Modifier.background(Color.Yellow),
-                fontSize = 24.sp
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        for (i in 1..3) {
+            Spacer(
+                modifier = Modifier
+                    .height(4.dp)
+                    .fillMaxWidth()
             )
-            DisableSelection {
-                Text(
-                    text = "This text is not selectable",
-                    modifier = Modifier.background(Color.Gray),
-                    fontSize = 24.sp
-                )
-            }
             Text(
-                text = "This text is selectable",
+                text = "Text $i",
                 modifier = Modifier.background(Color.Yellow),
                 fontSize = 24.sp
             )
