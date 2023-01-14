5263c391853e6cccdccc9e160f6f0ae2ef1bc362
Boris Precijan <borisprecijanbp@gmail.com>
2023-01-14 23:09:56 +0100

    Button Composable

--- app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
@@ -1,21 +1,24 @@
 package uns.pmf.learningjetpackcompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import uns.pmf.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,66 +26,40 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LearningJetpackComposeTheme {
-                Example1()
+                Example()
             }
         }
     }
 }
 
 @Composable
-fun Example1() {
-    val scrollState = rememberScrollState()
+fun Example() {
+    val context = LocalContext.current
     Row(
         modifier = Modifier
-            .fillMaxSize()
-            .horizontalScroll(scrollState),
-        verticalAlignment = Alignment.CenterVertically
+            .fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
     ) {
-        for (i in 0..30) {
-            Text(
-                text = "$i",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 8.dp),
-                fontSize = 36.sp,
-                fontWeight = FontWeight.Bold
-            )
+        Button(
+            onClick = {
+                Toast.makeText(context, "onClick", Toast.LENGTH_SHORT).show()
+            },
+            //enabled = false,
+            border = BorderStroke(2.dp, Color.Blue),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Red,
+                disabledBackgroundColor = Color.Green
+            ),
+            contentPadding = PaddingValues(all = 20.dp)
+        ) {
+            Text(text = "Button")
         }
     }
 }
 
-@Composable
-fun Example2() {
-    Row(
-        modifier = Modifier
-            .background(Color.Yellow)
-            .fillMaxSize(),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            text = "Item 1",
-            modifier = Modifier.background(Color.Red)
-        )
-        Text(
-            text = "Item 2",
-            modifier = Modifier.background(Color.Green)
-        )
-        Text(
-            text = "Item 3",
-            modifier = Modifier.background(Color.Blue)
-        )
-    }
-}
-
 @Preview(showBackground = true)
 @Composable
-fun Preview1() {
-    Example1()
-}
-
-@Preview(showBackground = true)
-@Composable
-fun Preview2() {
-    Example2()
+fun Preview() {
+    Example()
 }
