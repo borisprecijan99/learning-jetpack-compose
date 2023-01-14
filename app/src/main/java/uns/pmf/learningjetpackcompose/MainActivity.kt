3eb2c3a3cea510a17cb97dbf1c580e61cfae69b5
Boris Precijan <borisprecijanbp@gmail.com>
2023-01-14 22:18:12 +0100

    Column Composable

--- app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
@@ -5,10 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -19,34 +23,61 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LearningJetpackComposeTheme {
-                Example()
+                Example2()
             }
         }
     }
 }
 
 @Composable
-fun Example() {
+fun Example1() {
+    val scrollState = rememberScrollState()
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
     ) {
-        for (i in 1..3) {
-            Spacer(
-                modifier = Modifier
-                    .height(4.dp)
-                    .fillMaxWidth()
-            )
+        for (i in 0..30) {
             Text(
-                text = "Text $i",
-                modifier = Modifier.background(Color.Yellow),
-                fontSize = 24.sp
+                text = "$i",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(4.dp),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
             )
         }
     }
 }
 
+@Composable
+fun Example2() {
+    Column(
+        modifier = Modifier
+            .background(Color.Red)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Example 2",
+            modifier = Modifier.background(Color.Yellow)
+        )
+        Text(
+            text = "Column Composable",
+            modifier = Modifier.background(Color.Yellow)
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-fun ExamplePreview() {
-    Example()
+fun Preview1() {
+    Example1()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview2() {
+    Example2()
 }
