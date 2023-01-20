280579b4a15f21c2a6ef8147732acf3bee403d6d
Boris Precijan <borisprecijanbp@gmail.com>
2023-01-20 23:01:41 +0100

    Divider Composable

--- app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
@@ -3,13 +3,15 @@ package uns.pmf.learningjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import uns.pmf.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
@@ -27,102 +29,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Example() {
-    Row(
+    Column(
         modifier = Modifier
             .fillMaxSize(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceEvenly
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        var isOpenedDialog1 by remember {
-            mutableStateOf(false)
-        }
-        var isOpenedDialog2 by remember {
-            mutableStateOf(false)
-        }
-        var message by remember {
-            mutableStateOf("")
-        }
-
-        if (message.isNotEmpty()) {
-            Text(text = message)
-        }
-        Button(
-            onClick = {
-                isOpenedDialog1 = true
-            }
-        ) {
-            Text(text = "Dialog 1")
-        }
-        Button(
-            onClick = {
-                isOpenedDialog2 = true
-            }
-        ) {
-            Text(text = "Dialog 2")
-        }
-
-        if (isOpenedDialog1) {
-            AlertDialog(
-                onDismissRequest = {
-                    isOpenedDialog1 = false
-                },
-                confirmButton = {
-                    Button(
-                        onClick = {
-                            isOpenedDialog1 = false
-                            message = "D1: Confirm"
-                        }
-                    ) {
-                        Text(text = "Confirm")
-                    }
-                },
-                dismissButton = {
-                    Button(
-                        onClick = {
-                            isOpenedDialog1 = false
-                            message = "D1: Dismiss"
-                        }
-                    ) {
-                        Text(text = "Dismiss")
-                    }
-                },
-                title = {
-                    Text(text = "Title")
-                },
-                text = {
-                    Text(text = "Text")
-                }
-            )
-        }
-        if (isOpenedDialog2) {
-            AlertDialog(
-                onDismissRequest = {
-                    isOpenedDialog2 = false
-                },
-                title = {
-                    Text(text = "Title")
-                },
-                text = {
-                    Text(text = "Text")
-                },
-                buttons = {
-                    Row(
-                        modifier = Modifier.padding(all = 8.dp),
-                        horizontalArrangement = Arrangement.Center
-                    ) {
-                        Button(
-                            modifier = Modifier.fillMaxWidth(),
-                            onClick = {
-                                isOpenedDialog2 = false
-                                message = "D2: Dismiss"
-                            }
-                        ) {
-                            Text(text = "Dismiss")
-                        }
-                    }
-                }
-            )
-        }
+        Text(text = "Divider")
+        Divider(
+            color = Color.Red,
+            thickness = 3.dp,
+            startIndent = 30.dp
+        )
+        Text(text = "Composable")
     }
 }
 
