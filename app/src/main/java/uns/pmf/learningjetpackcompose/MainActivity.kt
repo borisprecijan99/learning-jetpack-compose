a55568e665065fbe91bfc31dfacab4f28e1f7940
Boris Precijan <borisprecijanbp@gmail.com>
2023-01-20 22:45:08 +0100

    AlertDialog Composable

--- app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
@@ -1,19 +1,17 @@
 package uns.pmf.learningjetpackcompose
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import uns.pmf.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,19 +27,101 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Example() {
-    Column(
+    Row(
         modifier = Modifier
             .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly
     ) {
-        val context = LocalContext.current
-        TextButton(
+        var isOpenedDialog1 by remember {
+            mutableStateOf(false)
+        }
+        var isOpenedDialog2 by remember {
+            mutableStateOf(false)
+        }
+        var message by remember {
+            mutableStateOf("")
+        }
+
+        if (message.isNotEmpty()) {
+            Text(text = message)
+        }
+        Button(
             onClick = {
-                Toast.makeText(context, "Toast message", Toast.LENGTH_SHORT).show()
+                isOpenedDialog1 = true
             }
         ) {
-            Text(text = "Text Button")
+            Text(text = "Dialog 1")
+        }
+        Button(
+            onClick = {
+                isOpenedDialog2 = true
+            }
+        ) {
+            Text(text = "Dialog 2")
+        }
+
+        if (isOpenedDialog1) {
+            AlertDialog(
+                onDismissRequest = {
+                    isOpenedDialog1 = false
+                },
+                confirmButton = {
+                    Button(
+                        onClick = {
+                            isOpenedDialog1 = false
+                            message = "D1: Confirm"
+                        }
+                    ) {
+                        Text(text = "Confirm")
+                    }
+                },
+                dismissButton = {
+                    Button(
+                        onClick = {
+                            isOpenedDialog1 = false
+                            message = "D1: Dismiss"
+                        }
+                    ) {
+                        Text(text = "Dismiss")
+                    }
+                },
+                title = {
+                    Text(text = "Title")
+                },
+                text = {
+                    Text(text = "Text")
+                }
+            )
+        }
+        if (isOpenedDialog2) {
+            AlertDialog(
+                onDismissRequest = {
+                    isOpenedDialog2 = false
+                },
+                title = {
+                    Text(text = "Title")
+                },
+                text = {
+                    Text(text = "Text")
+                },
+                buttons = {
+                    Row(
+                        modifier = Modifier.padding(all = 8.dp),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Button(
+                            modifier = Modifier.fillMaxWidth(),
+                            onClick = {
+                                isOpenedDialog2 = false
+                                message = "D2: Dismiss"
+                            }
+                        ) {
+                            Text(text = "Dismiss")
+                        }
+                    }
+                }
+            )
         }
     }
 }
