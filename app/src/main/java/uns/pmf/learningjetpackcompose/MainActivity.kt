edd3420b3347cd2eeeae6b06473ca7dd1ff6bf23
Boris Precijan <borisprecijanbp@gmail.com>
2023-01-20 18:42:39 +0100

    Checkbox Composable

--- app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
@@ -5,18 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Slider
+import androidx.compose.material.Button
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import uns.pmf.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,32 +29,40 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Example() {
-    val color = Color(120, 120, 255)
     Column(
         modifier = Modifier
             .fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceEvenly,
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        var sliderState by remember {
-            mutableStateOf(0.5f)
+        var isChecked by remember {
+            mutableStateOf(false)
         }
-        Text(
-            text = String.format("%.2f%%", sliderState * 100),
-            fontSize = 36.sp
-        )
-        CircularProgressIndicator(
-            progress = sliderState,
-            modifier = Modifier.size(100.dp),
-            color = color,
-            strokeWidth = 20.dp
-        )
-        Slider(
-            value = sliderState,
-            onValueChange = {
-                sliderState = it
+        var isEnabled by remember {
+            mutableStateOf(true)
+        }
+        Text(text = "Is checked: $isChecked")
+        Text(text = "Is enabled: $isEnabled")
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "Checkbox")
+            Checkbox(
+                checked = isChecked,
+                onCheckedChange = {
+                    isChecked = it
+                },
+                enabled = isEnabled
+            )
+        }
+        Button(
+            onClick = {
+                isEnabled = !isEnabled
             }
-        )
+        ) {
+            Text(text = if (isEnabled) "Disable" else "Enable")
+        }
     }
 }
 
