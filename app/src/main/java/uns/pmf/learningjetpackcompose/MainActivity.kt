30f7d148c30cf6ee21b3382f0560a828adc51648
Boris Precijan <borisprecijanbp@gmail.com>
2023-01-20 20:44:39 +0100

    OutlinedButton Composable

--- app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
@@ -1,19 +1,25 @@
 package uns.pmf.learningjetpackcompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.Checkbox
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import uns.pmf.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,33 +41,17 @@ fun Example() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        var isChecked by remember {
-            mutableStateOf(false)
-        }
-        var isEnabled by remember {
-            mutableStateOf(true)
-        }
-        Text(text = "Is checked: $isChecked")
-        Text(text = "Is enabled: $isEnabled")
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(text = "Checkbox")
-            Checkbox(
-                checked = isChecked,
-                onCheckedChange = {
-                    isChecked = it
-                },
-                enabled = isEnabled
-            )
-        }
-        Button(
+        val context = LocalContext.current
+        OutlinedButton(
             onClick = {
-                isEnabled = !isEnabled
-            }
+                Toast.makeText(context, "onClick", Toast.LENGTH_SHORT).show()
+            },
+            elevation = ButtonDefaults.elevation(defaultElevation = 10.dp),
+            shape = CircleShape,
+            border = BorderStroke(1.dp, Color.Yellow),
+            contentPadding = PaddingValues(16.dp)
         ) {
-            Text(text = if (isEnabled) "Disable" else "Enable")
+            Text(text = "Outlined Button")
         }
     }
 }
