d4c660756bdef93d0c034adf0f05874f1c285e20
Boris Precijan <borisprecijanbp@gmail.com>
2023-01-20 20:51:42 +0100

    TextButton Composable

--- app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
@@ -4,22 +4,16 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import uns.pmf.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,16 +36,12 @@ fun Example() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         val context = LocalContext.current
-        OutlinedButton(
+        TextButton(
             onClick = {
-                Toast.makeText(context, "onClick", Toast.LENGTH_SHORT).show()
-            },
-            elevation = ButtonDefaults.elevation(defaultElevation = 10.dp),
-            shape = CircleShape,
-            border = BorderStroke(1.dp, Color.Yellow),
-            contentPadding = PaddingValues(16.dp)
+                Toast.makeText(context, "Toast message", Toast.LENGTH_SHORT).show()
+            }
         ) {
-            Text(text = "Outlined Button")
+            Text(text = "Text Button")
         }
     }
 }
