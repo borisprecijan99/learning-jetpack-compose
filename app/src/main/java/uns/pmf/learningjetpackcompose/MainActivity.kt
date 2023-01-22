3be71834f3e78bfbb0c59c38a686befee84af9ff
Boris Precijan <borisprecijanbp@gmail.com>
2023-01-23 00:32:06 +0100

    Image Composable

--- app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
@@ -3,18 +3,22 @@ package uns.pmf.learningjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Icon
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import uns.pmf.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,15 +40,24 @@ fun Example() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Icon(
-            imageVector = Icons.Default.Favorite,
-            contentDescription = "Icon 1",
-            tint = Color.Red
-        )
-        Icon(
+        Image(
             painter = painterResource(id = R.drawable.ic_baseline_phone_android_24),
-            contentDescription = "Icon 2",
-            tint = Color.Blue
+            contentDescription = "First image",
+            modifier = Modifier.size(100.dp),
+            alignment = Alignment.CenterEnd,
+            contentScale = ContentScale.None,
+            alpha = 0.3f
+        )
+        Image(
+            imageVector = Icons.Default.Image,
+            contentDescription = "Second image",
+            modifier = Modifier.size(100.dp),
+            alignment = Alignment.TopStart,
+            contentScale = ContentScale.None
+        )
+        Image(
+            bitmap = ImageBitmap.imageResource(id = R.drawable.jetpack_compose_icon),
+            contentDescription = "Third image"
         )
     }
 }
