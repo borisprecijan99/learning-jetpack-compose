71674316c6e6f953d04917c495ad34e1f965b187
Boris Precijan <borisprecijanbp@gmail.com>
2023-01-22 22:35:25 +0100

    OutlinedTextField Composable

--- app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
@@ -3,24 +3,17 @@ package uns.pmf.learningjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import uns.pmf.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
 
@@ -35,59 +28,30 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun Example() {
-    var text by remember {
-        mutableStateOf("password")
+    var name by remember {
+        mutableStateOf("")
     }
-    var passwordVisibility by remember {
-        mutableStateOf(false)
-    }
-    val keyboardController = LocalSoftwareKeyboardController.current
-    val icon = if (passwordVisibility) Icons.Default.Visibility else Icons.Default.VisibilityOff
+
     Column(
         modifier = Modifier
             .fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        TextField(
-            value = text,
+        OutlinedTextField(
+            value = name,
             onValueChange = {
-                text = it
+                name = it
             },
-            //enabled = false,
-            //readOnly = true,
-            label = { Text(text = "Label") },
-            placeholder = { Text(text = "Placeholder") },
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Default.Password,
-                    contentDescription = "Leading icon"
-                )
-            },
-            trailingIcon = {
-                Icon(
-                    imageVector = icon,
-                    contentDescription = "Trailing icon",
-                    modifier = Modifier.clickable {
-                        passwordVisibility = !passwordVisibility
-                    }
-                )
-            },
-            isError = text.isEmpty(),
-            visualTransformation = if (passwordVisibility) VisualTransformation.None else PasswordVisualTransformation(),
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Password,
-                imeAction = ImeAction.Done
-            ),
-            keyboardActions = KeyboardActions(
-                onAny = {
-                    keyboardController?.hide()
-                }
-            ),
-            singleLine = true
+            label = { Text(text = "Name") },
+            placeholder = { Text(text = "Enter your name") },
+            singleLine = true,
+            shape = CircleShape,
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.Yellow
+            )
         )
     }
 }
