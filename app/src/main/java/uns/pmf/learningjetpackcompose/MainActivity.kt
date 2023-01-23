703df111052a06a55007b42c9eaee8238fb5cc5b
Boris Precijan <borisprecijanbp@gmail.com>
2023-01-23 16:17:49 +0100

    LinearProgressIndicator Composable

--- app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/uns/pmf/learningjetpackcompose/MainActivity.kt
@@ -3,20 +3,14 @@ package uns.pmf.learningjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import uns.pmf.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
@@ -40,25 +34,28 @@ fun Example() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_baseline_phone_android_24),
-            contentDescription = "First image",
-            modifier = Modifier.size(100.dp),
-            alignment = Alignment.CenterEnd,
-            contentScale = ContentScale.None,
-            alpha = 0.3f
-        )
-        Image(
-            imageVector = Icons.Default.Image,
-            contentDescription = "Second image",
-            modifier = Modifier.size(100.dp),
-            alignment = Alignment.TopStart,
-            contentScale = ContentScale.None
-        )
-        Image(
-            bitmap = ImageBitmap.imageResource(id = R.drawable.jetpack_compose_icon),
-            contentDescription = "Third image"
-        )
+        Column(
+            modifier = Modifier
+                .border(2.dp, Color.Black)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Indeterminate Linear Progress Indicator")
+            LinearProgressIndicator(
+                color = Color.DarkGray,
+                backgroundColor = Color.LightGray
+            )
+        }
+        Spacer(modifier = Modifier.height(100.dp))
+        Column(
+            modifier = Modifier
+                .border(2.dp, Color.Black)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Determinate Linear Progress Indicator")
+            LinearProgressIndicator(progress = 0.33f)
+        }
     }
 }
 
